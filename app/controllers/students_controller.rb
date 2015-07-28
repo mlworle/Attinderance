27b96e9a44b757f96ed759ba5ed4ce66d3822d50
mlworle <mlworle@gmail.com>
2015-07-28 15:54:23 -0400

    created two action/views: edit and delete delete and edit actions and pages created but not working well
    edit now shows new student page
    
    root changed to index page
    
    figured out how to reset id to 1 in ActiveRecord after destroy_all
    
    cosmetic changes to index view
    
    created empty files _edit.html.erb and _update.html.erb
    
    added confirmation for destroy
    
    destroy action does just that, but issue with POST
    
    redefined destroy action, but still getting Routing Error: 'No route matches [POST]/students/1'
    
    make suggested changes from pull request to student_controller and index.html.erb
    
    edit action added in students_controller
    
    redirect from edit to index now working
    
    removed unused forms

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,7 +5,23 @@ class StudentsController < ApplicationController
   end
 
   def edit
+    # Edit will delete the old data and replace with new
+    @student = Student.find(params[:id])
   end
+
+  def update
+    respond_to do |format|
+      @student = Student.find(params[:id])
+      if @student.update(student_params)
+        format.html { redirect_to students_path, notice: 'Student info was successfully updated.' }
+        format.json { render :show, status: :ok, location: @student }
+      else
+        format.html { render :edit }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
   def show
   end
@@ -19,11 +35,21 @@ class StudentsController < ApplicationController
 
     @student.save
     redirect_to students_path
-
   end
 
   def student_params
     params.require( :student).permit( :name)
+  end
+
+ # DELETE /student/1
+  # DELETE /student/1.json
+  def destroy
+    Student.find(params[:id]).destroy
+  #  respond_to do |format|
+  #    format.html { redirect_to student_url, notice: 'Student was successfully removed.' }
+  #    format.json { head :no_content }
+  #  end
+    redirect_to students_path
   end
 
 end
