b94adc9592091a964d462c0bc4ade2f8c5c677d7
mlworle <mlworle@gmail.com>
2015-08-06 14:41:02 -0400

    hopefully this will collect all the unstaged files

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
   Rails.application.routes.draw do
+  get 'teacher/new'
+
+  get 'teacher/edit'
+
+  get 'teacher/delete'
+
+  get 'teacher/view'
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
 
   root 'static_pages#home'
   resources :students
-  #  root 'students#index'
   end
