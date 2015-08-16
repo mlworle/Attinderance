809db6e6a5d189888ef6cdf9ef3a3ae8772041bf
mlworle <mlworle@gmail.com>
2015-08-16 18:51:30 -0400

    created action and views for courses; same as students and teachers courses controller created and model migrated
    changed to resources in routes
    
    index action up and running
    
    new action running with appropriate form
    
    edit action and view running
    
    side_bar updated with courses link
    
    staged uncommitted files
    
    edited ' g' from file

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,52 @@
+class CoursesController < ApplicationController
+
+def index
+    @courses = Course.all
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    @course.save
+    redirect_to courses_path
+  end
+
+  def course_params
+    params.require( :course).permit( :name)
+  end
+
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      @course = Course.find(params[:id])
+      if @course.update(course_params)
+        format.html { redirect_to courses_path, notice: 'Course info was successfully updated.' }
+        format.json { render :show, status: :ok, location: @course }
+      else
+        format.html { render :edit }
+        format.json { render json: @course.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  # DELETE /course/1
+  # DELETE /course/1.json
+  def destroy
+    Course.find(params[:id]).destroy
+    respond_to do |format|
+      format.html { redirect_to course_url, notice: 'Course was successfully removed.' }
+      format.json { head :no_content }
+    end
+    # redirect_to static_pages_home_path
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
   Rails.application.routes.draw do
-  resources :teachers
-  
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
 
   root 'static_pages#home'
   resources :students
-  resources :teachers 
+  resources :teachers
+  resources :courses 
  end
