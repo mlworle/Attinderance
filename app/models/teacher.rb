ba369e38dde64ba9307963a18e71c2b4ed91f819
mlworle <mlworle@gmail.com>
2015-12-22 16:10:56 -0500

    remove validation(:password, :uniqueness :true)

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -6,7 +6,7 @@ class Teacher < ActiveRecord::Base
   validates(:name, presence: true, length: {maximum: 50}, uniqueness: true)
 
   VALID_PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$\z/
-  validates(:password, presence: true, length: {maximum: 10}, uniqueness: true, format: { with: VALID_PASSWORD_REGEX})
+  validates(:password, presence: true, length: {maximum: 10}, format: { with: VALID_PASSWORD_REGEX})
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX })  
